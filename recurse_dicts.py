e4fec3a3827bd5dfe8b4bb7996dc9f51771ce49f
Jonathan Mickle <jmickle05@gmail.com>
2013-05-08 17:24:03 -0600

    Adding Recursive Dictionary function

---
+++ recurse_dicts.py
@@ -0,0 +1,27 @@
+"""
+@author: Jonathan Mickle
+@summary: Traverses a through multi level dictionaries to diff them and return the changes
+@param original: takes in the original unmodified JSON
+@param modified: Takes in the modified json file for comparison
+@return: a json dictionary of changes key-> value 
+"""
+def diffDict(original, modified):
+    if isinstance(original, dict) and isinstance(modified, dict):
+        changes = {}
+        for key, value in modified.iteritems():
+            if isinstance(value, dict):
+                innerDict = diffDict(original[key], modified[key])
+                if innerDict != {}:
+                    changes[key] = {}
+                    changes[key].update(innerDict)
+            else:
+                if original.has_key(key):
+                    if value != original[key]:
+                        changes[key] = value
+                else:
+                    changes[key] = value
+                
+                    
+        return changes        
+    else:
+        raise Exception('parameters must be a dictionary')
